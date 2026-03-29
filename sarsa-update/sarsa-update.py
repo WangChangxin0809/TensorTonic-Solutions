58dc086de704d9d8f1fc570cbcaaa7fb0bd24798
lambda-__-delta4477 <wangchangxin0809@gmail.com>
2026-03-29 20:07:34 +0800

    Add solution for sarsa-update

---
+++ sarsa-update/sarsa-update.py
@@ -0,0 +1,11 @@
+def sarsa_update(q_table, state, action, reward, next_state, next_action, alpha, gamma):
+    """
+    Perform one SARSA update and return the updated Q-table.
+    """
+    new_q_table = q_table.copy()
+
+    delta = reward + gamma * q_table[next_state][next_action] - q_table[state][action]
+
+    new_q_table[state][action] = q_table[state][action] + alpha * delta
+
+    return new_q_table
